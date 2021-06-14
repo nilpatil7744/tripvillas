5832cd7de0cfd335b7b1e302ef1df9c80bbaa58c
Abhishek Verma <77038651+abhi5iet@users.noreply.github.com>
2021-06-14 17:40:41 +0530

    added progress indicators

--- src/Redux/SingleHotel/reducer.js
+++ src/Redux/SingleHotel/reducer.js
@@ -3,7 +3,7 @@ import { GET_HOTEL_FAILURE, GET_HOTEL_REQUEST, GET_HOTEL_SUCCESS } from "./actio
 const initState = {
     data : [],
     isLoading: true,
-    isError: true,
+    isError: false,
 }
 
 export const hotelsReducer = (state = initState, {type, payload}) => {
@@ -12,7 +12,7 @@ export const hotelsReducer = (state = initState, {type, payload}) => {
             return {
                 ...state,
                 isLoading: true,
-                // isError: false,
+                isError: false,
             };
         case GET_HOTEL_SUCCESS:
             return {
