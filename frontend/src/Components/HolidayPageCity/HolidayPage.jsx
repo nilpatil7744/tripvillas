5646d88879e44890b9bef1bba030970ad50b6e4f
Abhishek Verma <77038651+abhi5iet@users.noreply.github.com>
2021-06-16 12:19:06 +0530

    removed errors

--- frontend/src/Components/HolidayPageCity/HolidayPage.jsx
+++ frontend/src/Components/HolidayPageCity/HolidayPage.jsx
@@ -12,7 +12,6 @@ import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import Loadingg from "../Loading/Loadingg";
-////////////////////
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
