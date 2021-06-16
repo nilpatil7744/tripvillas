f94911c3b3ff3daeacee8a4ca4939699fc39a708
Abhishek Verma <77038651+abhi5iet@users.noreply.github.com>
2021-06-16 22:12:44 +0530

    added sort function

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 const connect = () => {
-    return mongoose.connect(process.env.MONGODB_ATLAS_LINK , {
+    return mongoose.connect(process.env.MONGODB_ATLAS_LINK, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
@@ -36,12 +36,18 @@ const Hotel = mongoose.model('hotels', hotelSchema);
 
 app.get('/hotels', async (req, res) => {
     const city = req.query.city;
+    const sort = req.query.sort;
     if (city === undefined) {
         const hotels = await Hotel.find({}).lean().exec();
         res.status(200).json({ data: hotels });
     } else {
-        const hotels = await Hotel.find({ $or: [{ city: city }, { state: city }] }).lean().exec();
-        res.status(200).json({ data: hotels });
+        if (sort === undefined) {
+            const hotels = await Hotel.find({ $or: [{ city: city }, { state: city }] }).sort({ basePrice: 1 }).lean().exec();
+            res.status(200).json({ data: hotels });
+        } else {
+            const hotels = await Hotel.find({ $or: [{ city: city }, { state: city }] }).sort({ basePrice: -1 }).lean().exec();
+            res.status(200).json({ data: hotels });
+        }
     }
 });
 
